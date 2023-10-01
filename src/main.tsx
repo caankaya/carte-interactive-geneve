68b829e04c7a5f3da1c625779239aa74cb0f1e63
Canoral <kaya68@outlook.fr>
2023-10-01 11:11:28 +0200

    build: Configuration de react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App.tsx';
-import './styles/index.css';
+import { Provider } from 'react-redux';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={''}>
+      <Route errorElement={''}>
+        <Route index element={<App />} />
+        {/* Compléter le router avec d'autres pages à partir de là*/}
+        {/* Oubliez pas d'ajouter Outlet dans le composant Layout*/}
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
