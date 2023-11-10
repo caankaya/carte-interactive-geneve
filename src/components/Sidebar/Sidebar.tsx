41444b0fb37dab52a1b03309d3a4f08ae4679e3f
Canoral <kaya68@outlook.com>
2023-11-10 17:41:45 +0100

    fix: :lipstick: correction la responsive design de la barre lattérale

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
-import logo from '/logo.png';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { getThemeId, openAboutModal, toggleSidebar } from '../../redux/reducers/interaction';
-import { themes } from '../../data/themes';
-import { eventHistorial } from '../../data/event';
-import { personnes } from '../../data/personnes';
-import About from '../Modal/About';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard, faBookOpen, faFlag, faHouse, faImagePortrait } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import logo from "/logo.png";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import {
+  getThemeId,
+  openAboutModal,
+  toggleSidebar,
+} from "../../redux/reducers/interaction";
+import { themes } from "../../data/themes";
+import { eventHistorial } from "../../data/event";
+import { personnes } from "../../data/personnes";
+import About from "../Modal/About";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressCard,
+  faBookOpen,
+  faFlag,
+  faHouse,
+  faImagePortrait,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar2() {
   const dispatch = useAppDispatch();
@@ -41,49 +51,61 @@ function Sidebar2() {
   };
 
   return (
-    <div className={`Sidebar absolute z-[999] ${sidebar ? 'lg:w-96' : 'w-full'}`}>
+    <div
+      className={`Sidebar absolute z-[999] ${sidebar ? "lg:w-96" : "w-full"}`}
+    >
       {/* Menu Burger */}
       <button
         type="button"
         className={`lg:focus:outline-none absolute bg-white p-3 rounded-full mx-5 duration-1000 mt-5 ${
-          sidebar ? 'ml-[263px]' : ''
-        } ${sidebar ? 'lg:ml-[400px]' : ''}`}
+          sidebar ? "ml-[263px]" : ""
+        } ${sidebar ? "lg:ml-[400px]" : ""}`}
         onClick={() => {
           dispatch(toggleSidebar(sidebar));
         }}
       >
         <span
           className={`block h-1 w-6 bg-base-content rounded-full transition-all duration-1000 transform ${
-            sidebar ? 'rotate-45 translate-y-2' : ''
+            sidebar ? "rotate-45 translate-y-2" : ""
           }`}
         ></span>
         <span
           className={`block h-1 w-6 bg-base-content rounded-full mt-1 transition-all duration-1000 ${
-            sidebar ? 'opacity-0' : ''
+            sidebar ? "opacity-0" : ""
           }`}
         ></span>
         <span
           className={`block h-1 w-6 bg-base-content rounded-full mt-1 transition-all duration-1000 transform ${
-            sidebar ? '-rotate-45 -translate-y-2' : ''
+            sidebar ? "-rotate-45 -translate-y-2" : ""
           }`}
         ></span>
       </button>
 
       {/* Contenu de la barre latérale */}
       <aside
-        className={`h-screen overflow-y-auto fixed top-0 left-0 lg:w-96 w-64 ${!isPersonOpen ? 'flex flex-col' : ''} ${
-          sidebar ? 'lg:translate-x-0 duration-1000' : '-translate-x-full duration-1000'
+        className={`h-screen overflow-y-auto fixed top-0 left-0 lg:w-96 w-64 ${
+          !isPersonOpen ? "flex flex-col" : ""
+        } ${
+          sidebar
+            ? "lg:translate-x-0 duration-1000"
+            : "-translate-x-full duration-1000"
         } bg-white`}
       >
         {/* Titres */}
-        <h1 className={`collapse-title font-bold text-[#cd3030] text-2xl uppercase text-left mb-2`}>
+        <h1
+          className={`collapse-title font-bold text-[#cd3030] lg:text-lg uppercase text-left mb-2`}
+        >
           héritage raciste dans l’espace public de genève
         </h1>
 
         {/* Personnages Historiques */}
         <ul
           className={`collapse collapse-arrow max-h-[100px] overflow-hidden rounded-none border-t-2
-           ${isPersonOpen ? 'max-h-screen transition-all duration-500 ease-in-out' : 'max-h-0'}`}
+           ${
+             isPersonOpen
+               ? "max-h-screen transition-all duration-500 ease-in-out"
+               : "max-h-0"
+           }`}
         >
           <input
             type="radio"
@@ -93,9 +115,13 @@ function Sidebar2() {
             onChange={togglePerson}
             onClick={togglePerson}
           />
-          <h2 className="collapse-title font-bold">
+          <h2 className="collapse-title font-bold max-lg:text-sm">
             Personnages historiques
-            <FontAwesomeIcon icon={faImagePortrait} size="lg" style={{ marginLeft: '1rem' }} />
+            <FontAwesomeIcon
+              icon={faImagePortrait}
+              size="lg"
+              style={{ marginLeft: "1rem" }}
+            />
           </h2>
           {isPersonOpen &&
             personnes.map((personne, index) => (
@@ -108,7 +134,9 @@ function Sidebar2() {
         {/* Faits Historiques */}
         <ul
           className={`collapse collapse-arrow max-h-[100px] overflow-hidden rounded-none border-y-2 ${
-            isEventHistorielOpen ? 'max-h-screen transition-all duration-1000 ease-in-out' : 'max-h-0'
+            isEventHistorielOpen
+              ? "max-h-screen transition-all duration-500 ease-in-out"
+              : "max-h-0"
           }`}
         >
           <input
@@ -119,9 +147,9 @@ function Sidebar2() {
             onChange={toggleEventHistoriel}
             onClick={toggleEventHistoriel}
           />
-          <h2 className="collapse-title font-bold">
+          <h2 className="collapse-title font-bold max-lg:text-sm">
             Faits historiques
-            <FontAwesomeIcon icon={faBookOpen} style={{ marginLeft: '1rem' }} />
+            <FontAwesomeIcon icon={faBookOpen} style={{ marginLeft: "1rem" }} />
           </h2>
           {isEventHistorielOpen && (
             <>
@@ -137,7 +165,9 @@ function Sidebar2() {
         {/* Thèmes */}
         <ul
           className={`collapse collapse-arrow max-h-[100px] overflow-hidden  ${
-            isThemeOpen ? 'max-h-screen transition-all duration-1000 ease-in-out' : 'max-h-0'
+            isThemeOpen
+              ? "max-h-screen transition-all duration-500 ease-in-out"
+              : "max-h-0"
           }`}
         >
           <input
@@ -148,15 +178,17 @@ function Sidebar2() {
             onChange={toggleThemes}
             onClick={toggleThemes}
           />
-          <h2 className="collapse-title font-bold">
+          <h2 className="collapse-title font-bold max-lg:text-sm">
             Thèmes
-            <FontAwesomeIcon icon={faFlag} style={{ marginLeft: '1rem' }} />
+            <FontAwesomeIcon icon={faFlag} style={{ marginLeft: "1rem" }} />
           </h2>
           {isThemeOpen && (
             <>
               {themes.map((theme, index) => (
                 <li
-                  className={`m-2 pl-2 text-sm ${activeIndex === index ? 'active' : ''}`}
+                  className={`m-2 pl-2 text-sm ${
+                    activeIndex === index ? "active" : ""
+                  }`}
                   key={index}
                   onClick={() => {
                     dispatch(getThemeId(theme.id));
@@ -173,23 +205,33 @@ function Sidebar2() {
         {/* Accueil */}
         <ul className="collapse border-y-2 rounded-none">
           <h2 className="collapse-title font-bold">
-            <a href={'/'}>
+            <a href={"/"} className="max-lg:text-sm">
               Accueil
-              <FontAwesomeIcon icon={faHouse} style={{ marginLeft: '1rem' }} />
+              <FontAwesomeIcon icon={faHouse} style={{ marginLeft: "1rem" }} />
             </a>
           </h2>
         </ul>
 
         {/* About */}
         <ul className="collapse rounded-none border-b-2">
-          <h2 className="collapse-title font-bold first-letter:uppercase" onClick={openModal}>
+          <h2
+            className="collapse-title font-bold first-letter:uppercase max-lg:text-sm"
+            onClick={openModal}
+          >
             à propos
-            <FontAwesomeIcon icon={faAddressCard} style={{ marginLeft: '1rem' }} />
+            <FontAwesomeIcon
+              icon={faAddressCard}
+              style={{ marginLeft: "1rem" }}
+            />
           </h2>
         </ul>
 
         {/* Logo */}
-        <img src={logo} alt="Logo de la ville de Genève" className={`w-1/2 mt-auto ml-auto mr-2 mb-2`} />
+        <img
+          src={logo}
+          alt="Logo de la ville de Genève"
+          className={`w-1/2 mt-auto ml-auto mr-2 mb-2`}
+        />
       </aside>
 
       {/* Popup modal pour la page à propos */}
